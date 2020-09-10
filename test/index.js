a4d65c5fc516bec1b8faac02ec7e8d2b2085abb4
cn1001wang <goddess0910kk@gmail.com>
2020-09-10 20:22:21 +0800

    从本地仓库迁移过来

--- test/index.js
+++ test/index.js
@@ -1,28 +1,128 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var NodeType;
-(function (NodeType) {
-    NodeType[NodeType["A"] = 0] = "A";
-    NodeType[NodeType["B"] = 1] = "B";
-    NodeType[NodeType["C"] = 2] = "C";
-})(NodeType || (NodeType = {}));
-var Dog = /** @class */ (function () {
-    function Dog(dog) {
-        console.log(dog);
-        this.type = dog.type;
-        switch (this.type) {
-            case NodeType.A:
-                this.AInfo = { value: "A" };
-                break;
-            case NodeType.B:
-                this.BInfo = { data: 23 };
-                break;
-        }
-    }
-    Dog.prototype.say = function () {
-        console.log("say");
-    };
-    return Dog;
-}());
-exports.default = Dog;
-//# sourceMappingURL=index.js.map
+
+
+const FlowSchemeDefinition= require('../dist/index').default;
+
+let fs = new FlowSchemeDefinition();
+fs.add(new FlowSchemeNode({ nodeType: NodeType.开始 }));
+fs.add(new FlowSchemeNode({ nodeType: NodeType.审批人 }));
+fs.add(new FlowSchemeNode({ nodeType: NodeType.抄送人 }));
+let branchNode = new FlowSchemeNode({ nodeType: NodeType.分支 });
+fs.add(branchNode);
+branchNode.getBranch(0).add(new FlowSchemeNode({ nodeType: NodeType.审批人 }));
+branchNode.getBranch(0).add(new FlowSchemeNode({ nodeType: NodeType.抄送人 }));
+
+
+  // public class FlowSchemeNode
+  // {
+  //     /// <summary>
+  //     /// 节点类型
+  //     /// </summary>
+  //     public NodeType NodeType { get; set; }
+  //     /// <summary>
+  //     /// 审批人数据
+  //     /// </summary>
+  //     public VerifyNodeInfo VerifyNodeInfo { get; set; }
+  //     /// <summary>
+  //     /// 抄送人数据
+  //     /// </summary>
+  //     public CCNodeInfo CCNodeInfo { get; set; }
+  //     /// <summary>
+  //     /// 分支数据
+  //     /// </summary>
+  //     public List<BranchNodeInfo> BranchNodeInfos { get; set; }
+  // }
+  
+  // public class BranchNodeInfo
+  // {
+  //     /// <summary>
+  //     /// 分支条件
+  //     /// </summary>
+  //     public string Condition { get; set; }
+  //     /// <summary>
+  //     /// 分支各节点
+  //     /// </summary>
+  //     public List<FlowSchemeNode> SubNodes { get; set; }
+  // }
+  
+  // public class VerifyNodeInfo
+  // {
+  //     /// <summary>
+  //     /// 审批方式
+  //     /// </summary>
+  //     public VerifyMethod VerifyMethod { get; set; }
+  //     /// <summary>
+  //     /// 审批人选择类型
+  //     /// </summary>
+  //     public VerifierChooseType VerifierChooseType { get; set; }
+  //     /// <summary>
+  //     /// 审批人数据
+  //     /// </summary>
+  //     public string Value { get; set; }
+  // }
+  
+  // public class CCNodeInfo
+  // {
+  //     public boolean AllowAdd { get; set; }
+  //     public string Value { get; set; }
+  // }
+  
+  // const nodes = [
+  //   { NodeType: NodeType.开始 },
+  //   {
+  //     NodeType: NodeType.审批人,
+  //     VerifyNodeInfo: {
+  //       VerifyMethod: VerifyMethod.会签,
+  //       VerifierChooseType: VerifierChooseType.指定用户,
+  //       Value: [],
+  //     },
+  //   },
+  //   {
+  //     NodeType: NodeType.分支,
+  //     BranchNodeInfos: [
+  //       {
+  //         Condition: "ProcessTypeName=='数控铣'",
+  //         SubNodes: [
+  //           {
+  //             NodeType: NodeType.审批人,
+  //             VerifyNodeInfo: {
+  //               VerifyMethod: VerifyMethod.会签,
+  //               VerifierChooseType: VerifierChooseType.指定用户,
+  //               Value: "1,2,3",
+  //             },
+  //           },
+  //           {
+  //             NodeType: NodeType.审批人,
+  //             VerifyNodeInfo: {
+  //               VerifyMethod: VerifyMethod.依次审批,
+  //               VerifierChooseType: VerifierChooseType.指定角色,
+  //               Value: "1,2,3",
+  //             },
+  //           },
+  //         ],
+  //       },
+  //       {
+  //         Condition: "ProcessTypeName=='数控铣'",
+  //         SubNodes: [
+  //           {
+  //             NodeType: NodeType.审批人,
+  //             VerifyNodeInfo: {
+  //               VerifyMethod: VerifyMethod.会签,
+  //               VerifierChooseType: VerifierChooseType.指定用户,
+  //               Value: "1,2,3",
+  //             },
+  //           },
+  //           {
+  //             NodeType: NodeType.审批人,
+  //             VerifyNodeInfo: {
+  //               VerifyMethod: VerifyMethod.依次审批,
+  //               VerifierChooseType: VerifierChooseType.指定角色,
+  //               Value: "1,2,3",
+  //             },
+  //           },
+  //         ],
+  //       },
+  //     ],
+  //   },
+  // ];
+  // console.log(nodes);
+  
